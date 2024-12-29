7d394214a08145fccd3e47127cc03ec2985284d1
Father1993 <enjoyhillol@gmail.com>
2024-12-30 09:28:49 +1000

    write...

---
+++ utils/__init__.py
@@ -0,0 +1,23 @@
+from .keyboards import (
+    get_main_menu_keyboard,
+    get_catalog_countries_keyboard,
+    get_car_selection_keyboard,
+    get_body_type_keyboard,
+    get_car_actions_keyboard,
+    get_survey_start_keyboard,
+    get_survey_budget_keyboard,
+    get_return_menu_keyboard,
+    get_navigation_keyboard
+)
+
+__all__ = [
+    'get_main_menu_keyboard',
+    'get_catalog_countries_keyboard',
+    'get_car_selection_keyboard',
+    'get_body_type_keyboard',
+    'get_car_actions_keyboard',
+    'get_survey_start_keyboard',
+    'get_survey_budget_keyboard',
+    'get_return_menu_keyboard',
+    'get_navigation_keyboard'
+]
